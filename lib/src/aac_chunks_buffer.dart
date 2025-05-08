888ed7ddbcbc773a1d98a018673added956d9b63
Yaroslav Halivets <yarikgalivetc@gmail.com>
2025-05-08 14:24:01 +0200

    change way of chunking

--- lib/src/aac_chunks_buffer.dart
+++ lib/src/aac_chunks_buffer.dart
@@ -5,64 +5,40 @@ typedef AudioChunk = Uint8List;
 
 /// A class that buffers AAC audio chunks and extracts complete ADTS frames
 final class AacChunksBuffer {
-  var _buffer = <int>[];
+  static const _adtsHeaderLength = 7;
+
+  final _buffer = <int>[];
 
   /// Adds a chunk of audio data to the buffer and extracts complete ADTS frames
-  List<AudioChunk> addChunk(AudioChunk chunk) {
+  AudioChunk addChunk(AudioChunk chunk) {
     _buffer.addAll(chunk);
 
-    final extractedFrames = <AudioChunk>[];
-    var bufferIndex = 0;
+    final frames = <int>[];
 
-    while (bufferIndex <= _buffer.length - 7) {
-      final headerIndex =
-          _findNextAdtsHeaderIndex(_buffer.sublist(bufferIndex));
+    while (_buffer.length >= _adtsHeaderLength) {
+      // Check for ADTS syncword (0xFFF)
+      if (!(_buffer[0] == 0xFF && (_buffer[1] & 0xF0) == 0xF0)) {
+        // Not a valid header; discard until next possible header
+        _buffer.removeAt(0);
+        continue;
+      }
 
-      if (headerIndex == -1) {
+      // Extract frame length (13 bits) from ADTS header
+      final frameLength = ((_buffer[3] & 0x03) << 11) |
+          (_buffer[4] << 3) |
+          ((_buffer[5] & 0xE0) >> 5);
+
+      if (_buffer.length < frameLength) {
+        // Wait for more data
         break;
       }
 
-      final absoluteHeaderIndex = bufferIndex + headerIndex;
-
-      if (_buffer.length < absoluteHeaderIndex + 7) {
-        break;
-      }
-
-      final frameLength = _getAdtsFrameLength(_buffer, absoluteHeaderIndex);
-
-      if (_buffer.length >= absoluteHeaderIndex + frameLength) {
-        final frame = Uint8List.fromList(
-          _buffer.sublist(
-            absoluteHeaderIndex,
-            absoluteHeaderIndex + frameLength,
-          ),
-        );
-
-        extractedFrames.add(frame);
-        bufferIndex = absoluteHeaderIndex + frameLength;
-      } else {
-        break;
-      }
+      // Extract full frame and add to output
+      frames.addAll(_buffer.sublist(0, frameLength));
+      _buffer.removeRange(0, frameLength);
     }
 
-    _buffer = _buffer.sublist(bufferIndex);
-
-    return extractedFrames;
-  }
-
-  int _getAdtsFrameLength(List<int> data, int index) {
-    return ((data[index + 3] & 0x03) << 11) |
-        ((data[index + 4] & 0xFF) << 3) |
-        ((data[index + 5] & 0xE0) >> 5);
-  }
-
-  int _findNextAdtsHeaderIndex(List<int> data) {
-    for (var i = 0; i <= data.length - 7; i++) {
-      if (data[i] == 0xFF && (data[i + 1] & 0xF0) == 0xF0) {
-        return i;
-      }
-    }
-    return -1;
+    return Uint8List.fromList(frames);
   }
 
   /// Clears the buffer
